60d1b39b0ef9fe53cf6e118d7507221480cc1d00
Paul J. Shaw <pavlvsxavier@gmail.com>
2021-04-06 20:33:18 +0100

    Implemented the modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,9 +2,12 @@ import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
+import Modal from './modules/Modal'
 
 let mobileMenu = new MobileMenu()
 let stickyHeader = new StickyHeader()
+new Modal()
+
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75)
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 70)
 
